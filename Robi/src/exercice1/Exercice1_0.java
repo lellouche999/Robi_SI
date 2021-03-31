abcb7d6965b79fffc5d8e56ec015b3dd6e38c104
lellouche999 <81638922+lellouche999@users.noreply.github.com>
2021-03-31 09:24:32 +0200

    Update Exercice1_0.java

--- Robi/src/exercice1/Exercice1_0.java
+++ Robi/src/exercice1/Exercice1_0.java
@@ -1,8 +1,10 @@
 package exercice1;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import graphicLayer.GRect;
 import graphicLayer.GSpace;
+import tools.Tools;
 
 public class Exercice1_0 {
 	GSpace space = new GSpace("Exercice 1", new Dimension(200, 150));
@@ -11,6 +13,60 @@ public class Exercice1_0 {
 	public Exercice1_0() {
 		space.addElement(robi);
 		space.open();
+		int width ;
+		int height;
+		
+		while(true) {
+			width = space.getWidth();
+			// Passage du coin haut gauche à droit 
+			while (robi.getX() < width-robi.getWidth() ) {
+				Tools.sleep(8);
+				width = space.getWidth();
+				height = space.getHeight();
+				robi.setX(robi.getX()+1);
+				robi.setY(0);
+				/* Changement de couleur avec une couleur aléatoire */
+				robi.setColor(new Color((int) (Math.random() * 0x1000000)));
+			}
+			
+			
+			height = space.getHeight();
+			
+			// Passage du coin haut droit à bas droit
+			
+			while(robi.getY() < height-robi.getHeight()) {
+				Tools.sleep(8);
+				width = space.getWidth();
+				height = space.getHeight();
+				robi.setY(robi.getY()+1);
+				robi.setX(width-robi.getWidth());
+				/* Changement de couleur avec une couleur aléatoire */
+				robi.setColor(new Color((int) (Math.random() * 0x1000000)));
+			}
+			
+			//Passage du bas droit à bas gauche
+			
+			while(robi.getX() > 0) {
+				Tools.sleep(8);
+				width = space.getWidth();
+				height = space.getHeight();width = space.getWidth();
+				robi.setX(robi.getX()-1);
+				robi.setY(height-robi.getHeight());
+				robi.setColor(new Color((int) (Math.random() * 0x1000000)));
+			}
+			
+			//Passage du bas gauche à haut droit
+			
+			while(robi.getY() > 0) {
+				Tools.sleep(8);
+				width = space.getWidth();
+				height = space.getHeight();
+				robi.setY(robi.getY()-1);
+				robi.setX(0);
+				robi.setColor(new Color((int) (Math.random() * 0x1000000)));
+			}
+			
+		}
 	}
 
 	public static void main(String[] args) {
