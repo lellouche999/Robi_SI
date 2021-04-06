250f1fa9af748b2fed2b7dad3a5ed359ceba3242
lellouche999 <81638922+lellouche999@users.noreply.github.com>
2021-04-06 17:41:17 +0200

    Update Exercice3_0.java

--- Robi/src/exercice3/Exercice3_0.java
+++ Robi/src/exercice3/Exercice3_0.java
@@ -2,7 +2,9 @@ package exercice3;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Point;
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.util.Iterator;
 import java.util.List;
 
@@ -58,6 +60,50 @@ public class Exercice3_0 {
 	}
 
 	Command getCommandFromExpr(SNode expr) {
+		//analyseur 
+		SNode identifier, action, arg1, arg2;
+		expr.children().forEach(e -> System.out.print(e.contents() + " "));
+		System.out.println();
+		identifier = expr.children().get(0);
+		if(identifier.contents().compareTo("space") == 0) {
+			action = identifier = expr.children().get(1);
+			if(action.contents().compareTo("setColor") == 0) {
+				arg1 = expr.children().get(2);
+				Color color;
+				try {
+					//pour changer la coulur de space 
+					Field field = Class.forName("java.awt.Color").getField(arg1.contents());
+					color = (Color)field.get(null);
+				} catch (Exception e) {
+					color = Color.white;
+				}
+				return new SpaceChangeColor(color);
+			} else if(action.contents().compareTo("sleep") == 0) {
+				arg1 = expr.children().get(2);
+				int time = Integer.valueOf(arg1.contents());
+				return new SpaceSleep(time);
+			}
+		} else if(identifier.contents().compareTo("robi") == 0) {
+			action = identifier = expr.children().get(1);
+			if(action.contents().compareTo("setColor") == 0) {
+				arg1 = expr.children().get(2);
+				Color color;
+				try {
+					Field field = Class.forName("java.awt.Color").getField(arg1.contents());
+					color = (Color)field.get(null);
+				} catch (Exception e) {
+					color = Color.white;
+				}
+				return new RobiChangeColor(color);
+				
+			} else if(action.contents().compareTo("translate") == 0) {
+				arg1 = expr.children().get(2);
+				arg2 = expr.children().get(3);
+				int x = Integer.valueOf(arg1.contents());
+				int y = Integer.valueOf(arg2.contents());
+				return new RobiTranslate(x, y);
+			}
+		}
 		return null;
 	}
 
@@ -68,10 +114,10 @@ public class Exercice3_0 {
 	public interface Command {
 		abstract public void run();
 	}
-
+	
+	//cette class permet de changer la couleur de space
 	public class SpaceChangeColor implements Command {
-		Color newColor;
-
+		private Color newColor;
 		public SpaceChangeColor(Color newColor) {
 			this.newColor = newColor;
 		}
@@ -82,4 +128,50 @@ public class Exercice3_0 {
 		}
 
 	}
+	
+	//cette class permet mettre space en pause pour une durée prédéfini 
+	public class SpaceSleep implements Command {
+		int timeOut;
+		public SpaceSleep(int timeToSleep) {
+			this.timeOut = timeToSleep;
+		}
+
+		@Override
+		public void run() {
+			try {
+				Thread.sleep(timeOut);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	//cette class permet de changer la couleur de Robi
+	public class RobiChangeColor implements Command {
+		Color newColor;
+		public RobiChangeColor(Color newColor) {
+			this.newColor = newColor;
+		}
+
+		@Override
+		public void run() {
+			robi.setColor(newColor);
+		}
+	}
+	
+	//cette class permet de déplacer le   Robi
+	public class RobiTranslate implements Command {
+		private Point p;
+		public RobiTranslate(int x, int y) {
+			this.p = new Point(robi.getX() + x, robi.getY() + y);
+		}
+		@Override
+		public void run() {
+			robi.setPosition(p);
+		}
+		
+	}
+
+	
+	
 }
