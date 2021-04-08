83583736019e778e7b26fde00d51917b6cff8568
lellouche999 <81638922+lellouche999@users.noreply.github.com>
2021-04-08 11:21:10 +0200

    Update Exercice2_1_0.java
    ivan

--- Robi/src/exercice2/Exercice2_1_0.java
+++ Robi/src/exercice2/Exercice2_1_0.java
@@ -9,23 +9,31 @@ import graphicLayer.GRect;
 import graphicLayer.GSpace;
 import stree.parser.SNode;
 import stree.parser.SParser;
+import tools.Tools;
 
 
-public class Exercice2_1_0 {
+public class Exercice2_1_0
+{
 	GSpace space = new GSpace("Exercice 2_1", new Dimension(200, 100));
 	GRect robi = new GRect();
+	//script sous forme de chaine de caractere
 	String script = "(space setColor black) (robi setColor yellow)";
 
-	public Exercice2_1_0() {
+	public Exercice2_1_0()
+	{
+		//Création de la fenetre
 		space.addElement(robi);
 		space.open();
 		this.runScript();
 	}
 
-	private void runScript() {
+	private void runScript()
+	{
 		SParser<SNode> parser = new SParser<>();
 		List<SNode> rootNodes = null;
 		try {
+			//Le script sous forme de chaine de caractère est transformé en
+			//arbre de SNode
 			rootNodes = parser.parse(script);
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -37,11 +45,43 @@ public class Exercice2_1_0 {
 	}
 	
 	private void run(SNode expr) {
-		// A compléter...
+		SNode identifier, action, arg1;
+		//affichage du contenu des noeuds
+		expr.children().forEach(a -> System.out.print(a.contents() + " "));
+		//Selection du premier noeud
+		identifier = expr.children().get(0);
+		//Si le contenu du premier noeud est "space"
+		if(identifier.contents().compareTo("space") == 0)
+		{
+			//Le contenu du second noeud correspond à l'action
+			action = expr.children().get(1);
+			//Si l'action est "setColor"
+			if(action.contents().compareTo("setColor") == 0)
+			{
+				arg1 = expr.children().get(2);
+				//On utilise le contenu du 3eme noeud avec la méthode setColor
+				//pour changer la couleur de "space"
+				space.setColor(Tools.getColorByName(arg1.contents()));
+			}
+			
+		}
+		//Si le contenu du premier noeud est "space"
+		else if(identifier.contents().compareTo("robi") == 0)
+		{
+			//Le contenu du second noeud correspond à l'action
+			action = expr.children().get(1);
+			//Si l'action est "setColor"
+			if(action.contents().compareTo("setColor") == 0)
+			{
+				arg1 = expr.children().get(2);
+				//On utilise le contenu du 3eme noeud avec la méthode setColor
+				//pour changer la couleur de "robi-"
+				robi.setColor(Tools.getColorByName(arg1.contents()));
+			}
+		}
 	}
 
 	public static void main(String[] args) {
 		new Exercice2_1_0();
 	}
-
 }
